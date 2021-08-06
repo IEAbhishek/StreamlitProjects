43b5d5c793a0497af94b8f097d54836048a469e2
Abhishek Singh <65626956+IEAbhishek@users.noreply.github.com>
2021-08-06 09:09:36 +0530

    Update irisApp.py

--- irisApp.py
+++ irisApp.py
@@ -20,3 +20,21 @@ def user_input_features():
             'petal_width': petal_width}
     features = pd.DataFrame(data, index=[0])
     return features
+
+df = user_input_features()
+st.subheader('User Input parameters')
+st.write(df)
+iris = datasets.load_iris()
+X = iris.data
+Y = iris.target
+clf = RandomForestClassifier()
+clf.fit(X, Y)
+prediction = clf.predict(df)
+prediction_proba = clf.predict_proba(df)
+
+st.subheader('Class labels and their corresponding index number')
+st.write(iris.target_names)
+st.subheader('Prediction')
+st.write(iris.target_names[prediction])
+st.subheader('Prediction Probability')
+st.write(prediction_proba)
