4ad4a88cbe330bc0769ba2553f9aba30644747dd
Abhishek Singh <65626956+IEAbhishek@users.noreply.github.com>
2021-08-09 09:28:22 +0530

    Update solubility-app.py

--- solubility-app.py
+++ solubility-app.py
@@ -17,3 +17,45 @@ def AromaticProportion(m):
   HeavyAtom = Descriptors.HeavyAtomCount(m)
   AR = AromaticAtom/HeavyAtom
   return AR
+def generate(smiles, verbose=False):
+
+    moldata= []
+    for elem in smiles:
+        mol=Chem.MolFromSmiles(elem)
+        moldata.append(mol)
+
+    baseData= np.arange(1,1)
+    i=0
+    for mol in moldata:
+
+        desc_MolLogP = Descriptors.MolLogP(mol)
+        desc_MolWt = Descriptors.MolWt(mol)
+        desc_NumRotatableBonds = Descriptors.NumRotatableBonds(mol)
+        desc_AromaticProportion = AromaticProportion(mol)
+
+        row = np.array([desc_MolLogP,
+                        desc_MolWt,
+                        desc_NumRotatableBonds,
+                        desc_AromaticProportion])
+
+        if(i==0):
+            baseData=row
+        else:
+            baseData=np.vstack([baseData, row])
+        i=i+1
+
+    columnNames=["MolLogP","MolWt","NumRotatableBonds","AromaticProportion"]
+    descriptors = pd.DataFrame(data=baseData,columns=columnNames)
+
+    return descriptors
+
+# Page Title
+
+image = Image.open('solubility-logo.jpg')
+st.image(image, use_column_width=True)
+st.write("""
+# Molecular Solubility Prediction Data obtained from the John S. Delaney. [ESOL:  Estimating Aqueous Solubility Directly from Molecular Structure](https://pubs.acs.org/doi/10.1021/ci034243x). ***J. Chem. Inf. Comput. Sci.*** 2004, 44, 3, 1000-1005.
+***
+""")
+
+st.sidebar.header('User Input Features')
